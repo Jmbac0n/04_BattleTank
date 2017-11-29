7949c358bb87e89c415feac9d761a7a24c79b880
DESKTOP-2HJDC7D\user <jakematthias@hotmail.com>
2017-11-29 20:01:19 +0000

    BE28 FindingPlayerController in C++

---
+++ BattleTank/Source/BattleTank/TankAIController.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto PlayerTank = GetPlayerTank();
+
+	if (!PlayerTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController can't find player tank"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AIController found player: %s"), *PlayerTank->GetName());
+	}
+
+}
+
+ATank* ATankAIController::GetAIControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
+
+	if (!PlayerTank)
+	{
+		return nullptr;
+	}
+	else
+	{
+		return Cast<ATank>(PlayerTank);
+	}
+}
+
